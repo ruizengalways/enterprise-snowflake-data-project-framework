c37afd49e987bb1dee8df02788ecbb24aa21b93a
Rui Zeng <ruizeng.always@outlook.com>
2026-08-29 17:21:03 +1000

    refactor: use Stream repeatable reads instead of affected-key work table

--- dbt_package/macros/scd/scd2_stream_task.sql
+++ dbt_package/macros/scd/scd2_stream_task.sql
@@ -1,21 +1,5 @@
-{% macro esf_scd2_affected_keys_table_sql(
-    affected_keys_relation,
-    event_relation,
-    key_columns
-) -%}
-    {%- set keys = enterprise_snowflake_framework.esf_require_columns(key_columns, 'key_columns') -%}
-create transient table if not exists {{ affected_keys_relation }} as
-select distinct
-    {%- for key in keys %}
-    {{ adapter.quote(key) }}{% if not loop.last %}, {% endif %}
-    {%- endfor %}
-from {{ event_relation }}
-where 1 = 0
-{%- endmacro %}
-
 {% macro esf_scd2_stream_task_body_sql(
     stream_relation,
-    affected_keys_relation,
     target_relation,
     event_relation,
     key_columns,
@@ -26,18 +10,23 @@ where 1 = 0
     delete_values=[]
 ) -%}
     {%- set keys = enterprise_snowflake_framework.esf_require_columns(key_columns, 'key_columns') -%}
+    {%- set affected_key_query -%}
+select distinct
+    {%- for key in keys %}
+    {{ adapter.quote(key) }}{% if not loop.last %}, {% endif %}
+    {%- endfor %}
+from {{ stream_relation }}
+    {%- endset -%}
 begin
     begin transaction;
 
-    insert overwrite into {{ affected_keys_relation }}
-    select distinct
-        {%- for key in keys %}
-        {{ adapter.quote(key) }}{% if not loop.last %}, {% endif %}
-        {%- endfor %}
-    from {{ stream_relation }};
-
+    -- Snowflake Streams provide repeatable-read semantics inside this explicit
+    -- transaction, so both DML statements see the same change set. No shared
+    -- affected-key work table or framework Stream checkpoint is required.
     delete from {{ target_relation }} as target
-    using {{ affected_keys_relation }} as affected
+    using (
+        {{ affected_key_query }}
+    ) as affected
     where {{ enterprise_snowflake_framework.esf_equal_keys('target', 'affected', keys) }};
 
     insert into {{ target_relation }}
@@ -49,7 +38,8 @@ begin
         hash_column,
         operation_column,
         delete_values,
-        affected_keys_relation
+        none,
+        affected_key_query
     ) }};
 
     commit;
@@ -63,7 +53,6 @@ end
 {% macro esf_scd2_stream_task_sql(
     task_relation,
     stream_relation,
-    affected_keys_relation,
     target_relation,
     event_relation,
     warehouse,
@@ -83,7 +72,6 @@ end
     {%- set body_sql -%}
 {{ enterprise_snowflake_framework.esf_scd2_stream_task_body_sql(
     stream_relation,
-    affected_keys_relation,
     target_relation,
     event_relation,
     key_columns,
