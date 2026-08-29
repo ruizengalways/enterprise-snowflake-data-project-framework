cd735c297711b619dd7fa6d452b0acd513591de9
Rui Zeng <ruizeng.always@outlook.com>
2026-08-29 16:39:11 +1000

    feat: add optional Dynamic Table SCD projections

---
+++ dbt_package/macros/scd/dynamic_tables.sql
@@ -0,0 +1,77 @@
+{% macro esf_scd1_dynamic_table_sql(
+    target_relation,
+    source_relation,
+    warehouse,
+    target_lag,
+    key_columns,
+    order_columns,
+    refresh_mode,
+    operation_column=none,
+    delete_values=[],
+    initialize='ON_SCHEDULE'
+) -%}
+    {%- set latest_sql -%}
+{{ enterprise_snowflake_framework.esf_latest_observation(
+    source_relation,
+    key_columns,
+    order_columns
+) }}
+    {%- endset -%}
+    {%- set select_sql -%}
+with latest as (
+    {{ latest_sql }}
+)
+select *
+from latest
+{%- if operation_column is not none and delete_values | length > 0 %}
+where {{ adapter.quote(operation_column) }} not in (
+    {%- for value in delete_values -%}
+    '{{ value | replace("'", "''") }}'{% if not loop.last %}, {% endif %}
+    {%- endfor -%}
+)
+{%- endif %}
+    {%- endset -%}
+{{ enterprise_snowflake_framework.esf_dynamic_table_projection_sql(
+    target_relation,
+    warehouse,
+    target_lag,
+    select_sql,
+    refresh_mode,
+    initialize
+) }}
+{%- endmacro %}
+
+{% macro esf_scd2_dynamic_table_sql(
+    target_relation,
+    event_relation,
+    warehouse,
+    target_lag,
+    key_columns,
+    effective_at_column,
+    order_columns,
+    hash_column,
+    refresh_mode,
+    operation_column=none,
+    delete_values=[],
+    initialize='ON_SCHEDULE'
+) -%}
+    {%- set history_sql -%}
+{{ enterprise_snowflake_framework.esf_scd2_event_history_select(
+    event_relation,
+    key_columns,
+    effective_at_column,
+    order_columns,
+    hash_column,
+    operation_column,
+    delete_values
+) }}
+    {%- endset -%}
+{{ enterprise_snowflake_framework.esf_dynamic_table_projection_sql(
+    target_relation,
+    warehouse,
+    target_lag,
+    history_sql,
+    refresh_mode,
+    initialize
+) }}
+{%- endmacro %}
