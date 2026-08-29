2baa1e6902a427cd4060c983d4777261ae0a61a3
Rui Zeng <ruizeng.always@outlook.com>
2026-08-29 16:21:41 +1000

    test: render Stream and triggered Task capture SQL

--- examples/dbt-smoke/macros/smoke_capture.sql
+++ examples/dbt-smoke/macros/smoke_capture.sql
@@ -16,6 +16,36 @@
 ) }}
     {%- endset -%}
 
-    {{ log('---LATEST---\n' ~ latest_sql ~ '\n---SNAPSHOT_DIFF---\n' ~ diff_sql, info=true) }}
+    {%- set stream_sql -%}
+{{ enterprise_snowflake_framework.esf_append_only_stream_sql(
+    'CI_HEALTH.PR_123_STAGING.PATIENT_EVENT_STREAM',
+    'CI_HEALTH.PR_123_STAGING.PATIENT_EVENT',
+    false,
+    'Consume immutable patient CDC events'
+) }}
+    {%- endset -%}
+
+    {%- set task_sql -%}
+{{ enterprise_snowflake_framework.esf_triggered_task_sql(
+    'CI_HEALTH.PR_123_STAGING.PROCESS_PATIENT_EVENT',
+    'CI_HEALTH.PR_123_STAGING.PATIENT_EVENT_STREAM',
+    'WH_HEALTH_CI',
+    'insert into CI_HEALTH.PR_123_STAGING.PATIENT_EVENT_CONSUMED select * from CI_HEALTH.PR_123_STAGING.PATIENT_EVENT_STREAM',
+    30,
+    3600000,
+    1,
+    3,
+    'Process immutable patient CDC events',
+    true
+) }}
+    {%- endset -%}
+
+    {{ log(
+        '---LATEST---\n' ~ latest_sql
+        ~ '\n---SNAPSHOT_DIFF---\n' ~ diff_sql
+        ~ '\n---APPEND_ONLY_STREAM---\n' ~ stream_sql
+        ~ '\n---TRIGGERED_TASK---\n' ~ task_sql,
+        info=true
+    ) }}
     {{ return('capture SQL rendered') }}
 {%- endmacro %}
