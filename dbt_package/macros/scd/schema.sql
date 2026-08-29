20f2443b3effd4dd82210c92b5eba92c08a1947f
Rui Zeng <ruizeng.always@outlook.com>
2026-08-29 16:41:33 +1000

    feat: add SCD2 target initialization helper

---
+++ dbt_package/macros/scd/schema.sql
@@ -0,0 +1,22 @@
+{% macro esf_scd2_target_table_sql(
+    target_relation,
+    source_relation,
+    transient=false,
+    comment=none
+) -%}
+    {%- if target_relation is not string or target_relation | trim == '' -%}
+        {{ exceptions.raise_compiler_error('target_relation must be a non-empty Snowflake relation') }}
+    {%- endif -%}
+    {%- if source_relation is not string or source_relation | trim == '' -%}
+        {{ exceptions.raise_compiler_error('source_relation must be a non-empty Snowflake table relation') }}
+    {%- endif -%}
+create {%- if transient %} transient{% endif %} table if not exists {{ target_relation }} like {{ source_relation }};
+
+alter table {{ target_relation }} add column if not exists _esf_valid_from timestamp_tz;
+alter table {{ target_relation }} add column if not exists _esf_valid_to timestamp_tz;
+alter table {{ target_relation }} add column if not exists _esf_is_current boolean;
+alter table {{ target_relation }} add column if not exists _esf_version_ordinal number(38, 0);
+{%- if comment is not none %}
+alter table {{ target_relation }} set comment = '{{ comment | replace("'", "''") }}';
+{%- endif %}
+{%- endmacro %}
