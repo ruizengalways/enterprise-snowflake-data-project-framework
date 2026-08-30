23d74e7e8b732db3047e358f3aa71702a9b0d706
Rui Zeng <ruizeng.always@outlook.com>
2026-08-30 12:58:37 +1000

    feat: split metadata-aware snapshot and event SCD2 APIs

--- dbt_package/macros/scd/metadata.sql
+++ dbt_package/macros/scd/metadata.sql
@@ -15,6 +15,17 @@
     {{ return(scd2) }}
 {%- endmacro %}
 
+{% macro esf_scd2_require_strategy(dataset_id, allowed_strategies) -%}
+    {%- set dataset = enterprise_snowflake_framework.esf_dataset_metadata(dataset_id) -%}
+    {%- set strategy = dataset.get('load_strategy') -%}
+    {%- if strategy not in allowed_strategies -%}
+        {{ exceptions.raise_compiler_error(
+            'dataset ' ~ dataset_id ~ ' uses ' ~ strategy ~ '; expected one of: ' ~ (allowed_strategies | join(', '))
+        ) }}
+    {%- endif -%}
+    {{ return(dataset) }}
+{%- endmacro %}
+
 {% macro esf_scd2_hash_expression(column_names, relation_alias=none) -%}
     {%- set columns = enterprise_snowflake_framework.esf_require_columns(column_names, 'tracked_columns') -%}
 hash(
@@ -24,23 +35,26 @@ hash(
 )
 {%- endmacro %}
 
-{% macro esf_scd2_enriched_event_relation(event_relation, dataset_id) -%}
+{% macro esf_scd2_enriched_relation(source_relation, dataset_id) -%}
     {%- set scd2 = enterprise_snowflake_framework.esf_scd2_dataset_metadata(dataset_id) -%}
 (
     select
-        source_events.*,
+        source_rows.*,
         {{ enterprise_snowflake_framework.esf_scd2_hash_expression(
             scd2.get('tracked_columns', []),
-            'source_events'
+            'source_rows'
         ) }} as _esf_record_hash
-    from {{ event_relation }} as source_events
+    from {{ source_relation }} as source_rows
 )
 {%- endmacro %}
 
 {% macro esf_scd2_history_select_for_dataset(event_relation, dataset_id) -%}
-    {%- set dataset = enterprise_snowflake_framework.esf_dataset_metadata(dataset_id) -%}
+    {%- set dataset = enterprise_snowflake_framework.esf_scd2_require_strategy(
+        dataset_id,
+        ['scd2_merge', 'scd2_stream_task']
+    ) -%}
     {%- set scd2 = enterprise_snowflake_framework.esf_scd2_dataset_metadata(dataset_id) -%}
-    {%- set enriched_relation = enterprise_snowflake_framework.esf_scd2_enriched_event_relation(
+    {%- set enriched_relation = enterprise_snowflake_framework.esf_scd2_enriched_relation(
         event_relation,
         dataset_id
     ) -%}
@@ -61,14 +75,17 @@ hash(
     affected_keys_relation,
     dataset_id
 ) -%}
-    {%- set dataset = enterprise_snowflake_framework.esf_dataset_metadata(dataset_id) -%}
+    {%- set dataset = enterprise_snowflake_framework.esf_scd2_require_strategy(
+        dataset_id,
+        ['scd2_merge', 'scd2_stream_task']
+    ) -%}
     {%- set scd2 = enterprise_snowflake_framework.esf_scd2_dataset_metadata(dataset_id) -%}
     {%- if scd2.get('late_arriving_policy') != 'rebuild_affected_keys' -%}
         {{ exceptions.raise_compiler_error(
             'dataset ' ~ dataset_id ~ ' does not allow affected-key rebuild for late-arriving events'
         ) }}
     {%- endif -%}
-    {%- set enriched_relation = enterprise_snowflake_framework.esf_scd2_enriched_event_relation(
+    {%- set enriched_relation = enterprise_snowflake_framework.esf_scd2_enriched_relation(
         event_relation,
         dataset_id
     ) -%}
@@ -82,5 +99,28 @@ hash(
     '_esf_record_hash',
     scd2.get('operation_column'),
     scd2.get('delete_values', [])
+) }}
+{%- endmacro %}
+
+{% macro esf_scd2_snapshot_apply_for_dataset_sql(
+    target_relation,
+    snapshot_relation,
+    dataset_id,
+    effective_at_expression='current_timestamp()'
+) -%}
+    {%- set dataset = enterprise_snowflake_framework.esf_scd2_require_strategy(
+        dataset_id,
+        ['scd2_snapshot']
+    ) -%}
+    {%- set enriched_relation = enterprise_snowflake_framework.esf_scd2_enriched_relation(
+        snapshot_relation,
+        dataset_id
+    ) -%}
+{{ enterprise_snowflake_framework.esf_scd2_snapshot_apply_sql(
+    target_relation,
+    enriched_relation,
+    dataset.get('business_key', []),
+    '_esf_record_hash',
+    effective_at_expression
 ) }}
 {%- endmacro %}
